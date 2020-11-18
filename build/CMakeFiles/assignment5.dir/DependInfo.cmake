893ba9e11b2c731730582363808da111366ec83d
Yanhao Li <hi@yanhaoli.com>
2020-11-18 11:50:57 -0500

    init

---
+++ build/CMakeFiles/assignment5.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/yanhaoli/Dropbox/university/nyu/classes/20fall/computer-graphic/rasterization/src/main.cpp" "/Users/yanhaoli/Dropbox/university/nyu/classes/20fall/computer-graphic/rasterization/build/CMakeFiles/assignment5.dir/src/main.cpp.o"
+  "/Users/yanhaoli/Dropbox/university/nyu/classes/20fall/computer-graphic/rasterization/src/raster.cpp" "/Users/yanhaoli/Dropbox/university/nyu/classes/20fall/computer-graphic/rasterization/build/CMakeFiles/assignment5.dir/src/raster.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "DATA_DIR=\"/Users/yanhaoli/Dropbox/university/nyu/classes/20fall/computer-graphic/rasterization/data/\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../ext/eigen"
+  "../ext/gif-h"
+  "../ext/stb"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
