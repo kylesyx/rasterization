54865a85a8e2e0b1a118ab760cf4b4f62b468461
Yanhao Li <hi@yanhaoli.com>
2020-11-30 10:48:11 -0500

    refactor: project structure

--- src/main.cpp
+++ src/main.cpp
@@ -5,66 +5,66 @@
 
 // Utilities for the Assignment
 #include "raster.h"
+#include "types.h"
+#include "load.h"
 
 // Image writing library
 #define STB_IMAGE_WRITE_IMPLEMENTATION // Do not include this line twice in your project!
 #include "stb_image_write.h"
 
-#include "gif.h"
-
 using namespace std;
 
-int main() 
+void render_scene(const Scene &scene) {
+	
+}
+
+int main(int argc, char *argv[]) 
 {
-
-	// The Framebuffer storing the image rendered by the rasterizer
-	Eigen::Matrix<FrameBufferAttributes,Eigen::Dynamic,Eigen::Dynamic> frameBuffer(500,500);
-
-	// Global Constants (empty in this example)
-	UniformAttributes uniform;
-
-	// Basic rasterization program
-	Program program;
-
-	// The vertex shader is the identity
-	program.VertexShader = [](const VertexAttributes& va, const UniformAttributes& uniform)
-	{
-		return va;
-	};
-
-	// The fragment shader uses a fixed color
-	program.FragmentShader = [](const VertexAttributes& va, const UniformAttributes& uniform)
-	{
-		return FragmentAttributes(1,0,0);
-	};
-
-	// The blending shader converts colors between 0 and 1 to uint8
-	program.BlendingShader = [](const FragmentAttributes& fa, const FrameBufferAttributes& previous)
-	{
-		return FrameBufferAttributes(fa.color[0]*255,fa.color[1]*255,fa.color[2]*255,fa.color[3]*255);
-	};
-
-	// One triangle in the center of the screen
-	vector<VertexAttributes> vertices;
-	vertices.push_back(VertexAttributes(-1,-1,0));
-	vertices.push_back(VertexAttributes(1,-1,0));
-	vertices.push_back(VertexAttributes(0,1,0));
-
-	const char * fileName = "triangle.gif";
-	vector<uint8_t> image;
-	int delay = 25;
-	GifWriter g;
-	GifBegin(&g, fileName, frameBuffer.rows(), frameBuffer.cols(), delay);
-
-	for (float i = 0; i < 1; i+= 0.2) {
-		frameBuffer.setConstant(FrameBufferAttributes());
-		vertices[2].position[1] -= 0.2;
-		rasterize_triangles(program,uniform,vertices,frameBuffer);
-		framebuffer_to_uint8(frameBuffer, image);
-		GifWriteFrame(&g, image.data(), frameBuffer.rows(), frameBuffer.cols(), delay);
+	if (argc < 2) {
+		std::cerr << "Usage: " << argv[0] << " scene.json" << std::endl;
+		return 1;
 	}
+	Scene scene = load_scene(argv[1]);
+	render_scene(scene);
 
-	GifEnd(&g);
+	// // The Framebuffer storing the image rendered by the rasterizer
+	// Eigen::Matrix<FrameBufferAttributes,Eigen::Dynamic,Eigen::Dynamic> frameBuffer(500,500);
 
+	// // Global Constants (empty in this example)
+	// UniformAttributes uniform;
+
+	// // Basic rasterization program
+	// Program program;
+
+	// // The vertex shader is the identity
+	// program.VertexShader = [](const VertexAttributes& va, const UniformAttributes& uniform)
+	// {
+	// 	return va;
+	// };
+
+	// // The fragment shader uses a fixed color
+	// program.FragmentShader = [](const VertexAttributes& va, const UniformAttributes& uniform)
+	// {
+	// 	return FragmentAttributes(1,0,0);
+	// };
+
+	// // The blending shader converts colors between 0 and 1 to uint8
+	// program.BlendingShader = [](const FragmentAttributes& fa, const FrameBufferAttributes& previous)
+	// {
+	// 	return FrameBufferAttributes(fa.color[0]*255,fa.color[1]*255,fa.color[2]*255,fa.color[3]*255);
+	// };
+
+	// // One triangle in the center of the screen
+	// vector<VertexAttributes> vertices;
+	// vertices.push_back(VertexAttributes(-1,-1,0));
+	// vertices.push_back(VertexAttributes(1,-1,0));
+	// vertices.push_back(VertexAttributes(0,1,0));
+
+	// rasterize_triangles(program,uniform,vertices,frameBuffer);
+
+	// vector<uint8_t> image;
+	// framebuffer_to_uint8(frameBuffer,image);
+	// stbi_write_png("triangle.png", frameBuffer.rows(), frameBuffer.cols(), 4, image.data(), frameBuffer.rows()*4);
+	
 	return 0;
 }
